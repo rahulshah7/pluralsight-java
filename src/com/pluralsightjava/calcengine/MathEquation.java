5ae30fd9cd4b6e5520a887657b37c05b34db6a05
Rahul Shah <rahulshah7@outlook.com>
2020-02-02 14:29:00 +0000

    Add accessors/mutators (getters/setters)

--- src/com/pluralsightjava/calcengine/MathEquation.java
+++ src/com/pluralsightjava/calcengine/MathEquation.java
@@ -1,10 +1,17 @@
 package com.pluralsightjava.calcengine;
 
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public void setLeftVal(double leftVal){this.leftVal = leftVal;}
+    public void setRightVal(double rightVal){this.rightVal = rightVal;}
+    public void setOpCode(char opCode){this.opCode = opCode;}
+
+    public double getResult(){return this.result;}
+
 
     public void execute() {
         switch (opCode) {
